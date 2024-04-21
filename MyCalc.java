d3dc9700f1a5876ca715df51aab5e5d26a459382
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-21 21:09:09 +0530

    sum() added

---
+++ MyCalc.java
@@ -0,0 +1,11 @@
+
+public class MyCalc {
+    int sum(int a, int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        MyCalc calc = new MyCalc();
+        System.out.println("Sum is "+calc.sum(20, 10));
+    }    
+}
